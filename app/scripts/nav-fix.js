ee19309610ad1ca2a73df7e87acf1748c6eed57f
Tyler Youngblood <tyleryoungblood@yahoo.com>
2014-07-23 12:20:08 -0400

    fix non-collapsing dropdown nav

---
+++ app/scripts/nav-fix.js
@@ -0,0 +1,12 @@
+/************************************************************
+* This js is used to fix the but with angular and bootstrap *
+* that causes the dropdown nav menu to remain open after a  *
+* link is clicked. Fix from kevinknelson's solution here:   *
+* https://github.com/twbs/bootstrap/issues/9013             *
+************************************************************/
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+});
